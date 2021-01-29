ca8e2922a0208f2ed8ff1f6e11346bb2f74c7f6a
Stefanie Stoppel <s.stoppel@gmx.de>
2021-01-29 13:35:45 +0100

    feat: load fine-tuned dogs vs cats model based on VGG16 and use it in attack; save original and adversarial images with original file names prefixed

--- src/attack_with_foolbox.py
+++ src/attack_with_foolbox.py
@@ -3,19 +3,17 @@ import pprint
 import eagerpy as ep
 import foolbox as fb
 import numpy as np
-import pandas as pd
 import torch
 import torchvision
-import torchvision.models as models
 
 from typing import Union, Sequence
 from eagerpy import Tensor
 from torch.utils.data import DataLoader
-from torchvision import datasets
 from torchvision.transforms import transforms
 
 from dataloader import get_dogs_vs_cats_data_splits
-from src.config import DATA_PATH
+from lit_model import LitVGG16Model
+from src.config import DATA_PATH, LOGS_PATH
 from src.util import timeit
 
 pp = pprint.PrettyPrinter(indent=4, width=120)
@@ -24,26 +22,26 @@ CONFIG = {
     # Directories
     "target_dir": os.path.join(DATA_PATH, "adversarials"),
     "train_dir": os.path.join(DATA_PATH, "train"),
-    "test_dir": os.path.join(DATA_PATH, "test"),
-    "train_csv": os.path.join(DATA_PATH, "train_list.csv"),
-    "test_csv": os.path.join(DATA_PATH, "test_list.csv"),
+    "checkpoint": os.path.join(LOGS_PATH, "default/version_1/checkpoints/epoch=0-step=136.ckpt"),
 
     # Attack
-    "epsilons": np.linspace(0.001, 0.005, num=1),
+    # "epsilons": np.linspace(0.001, 0.005, num=1),
+    "epsilons": [0.005],
     "bounds": (0, 1),
     "preprocessing": dict(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225], axis=-3),
     "attack": fb.attacks.LinfFastGradientAttack(),
     "save_adversaries": True,
 
     # Training
-    "validation_split": 0.2,
-    "batch_size": 64,
+    "data_split": (0.7, 0.2, 0.1),
+    "batch_size": 16,
     "num_workers": 2,
+    "use_cuda": True,
 }
 
 
-def wrap_model(model, bounds, preprocessing):
-    foolbox_model = fb.PyTorchModel(model, bounds=bounds, preprocessing=preprocessing)
+def wrap_model(model, bounds, preprocessing, device):
+    foolbox_model = fb.PyTorchModel(model, bounds=bounds, preprocessing=preprocessing, device=device)
     return foolbox_model
 
 
@@ -56,9 +54,13 @@ def attack_model(attack: fb.Attack,
     return attack(foolbox_model, images, labels, epsilons=epsilons)
 
 
-def filter_correctly_classified(foolbox_model: fb.PyTorchModel, images: Tensor, labels: ep.Tensor):
+def filter_correctly_classified(foolbox_model: fb.PyTorchModel,
+                                images: Tensor,
+                                labels: ep.Tensor,
+                                file_names: ep.Tensor):
     correctly_classified_mask = get_correctly_classified_mask(foolbox_model, images, labels)
-    return images[correctly_classified_mask], labels[correctly_classified_mask]
+    return images[correctly_classified_mask], labels[correctly_classified_mask], file_names[
+        correctly_classified_mask.cpu()]
 
 
 def get_correctly_classified_mask(foolbox_model: fb.PyTorchModel, images: ep.Tensor, labels: ep.Tensor):
@@ -76,13 +78,17 @@ def get_correctly_classified_mask(foolbox_model: fb.PyTorchModel, images: ep.Ten
     return restore_type_images(predictions == labels_)
 
 
-def run_attack(fb_attack: fb.Attack, foolbox_model: fb.PyTorchModel, images: torch.Tensor, labels: torch.Tensor,
+def run_attack(fb_attack: fb.Attack,
+               foolbox_model: fb.PyTorchModel,
+               images: torch.Tensor,
+               labels: torch.Tensor,
+               file_names: torch.Tensor,
                config):
     print(f"Initial accuracy on images: {fb.utils.accuracy(foolbox_model, images, labels)}")
 
     # filter input images and labels so that only the ones which are correctly classified are kept
     print(f"before filtering: {images.shape}; {labels.shape}")
-    images, labels = filter_correctly_classified(foolbox_model, images, labels)
+    images, labels, file_names = filter_correctly_classified(foolbox_model, images, labels, file_names)
     print(f"after filtering: {images.shape}; {labels.shape}")
     if images.nelement() == 0:
         raise SystemExit("SYSTEM_EXIT: No images left after filtering. Are you sure you trained your model correctly "
@@ -97,7 +103,9 @@ def run_attack(fb_attack: fb.Attack, foolbox_model: fb.PyTorchModel, images: tor
     for i, (eps, acc, clipped_adv) in enumerate(zip(epsilons, robust_accuracy, clipped)):
         print(f"!!!! Linf norm ≤ {eps:<6}: {acc.item() * 100:4.1f} %")
         adversarial_mask = torch.nonzero(is_adv.flatten()).flatten()
+        print(f"Amount of true adversarial images: {len(adversarial_mask)}.")
         original_images = images[adversarial_mask]
+        original_image_names = file_names[adversarial_mask.cpu()]
         adversarial_images = clipped_adv[adversarial_mask]
 
         original_pred = foolbox_model(original_images).argmax(axis=-1)
@@ -109,56 +117,68 @@ def run_attack(fb_attack: fb.Attack, foolbox_model: fb.PyTorchModel, images: tor
         pp.pprint(f"Adversarial:  {adv_pred}")
 
         if config["save_adversaries"]:
-            target_dir = os.path.join(config["target_dir"], fb_attack.__class__.__name__)
-            os.makedirs(target_dir, exist_ok=True)
-            print(f"Saving original & adversarial images in directory '{target_dir}'.")
-
-            for original_img, adv_img, image_index in zip(original_images, adversarial_images, adversarial_mask):
-                original_path = os.path.join(target_dir, f"{image_index}_orig_eps_{eps}.jpg")
-                adv_path = os.path.join(target_dir, f"{image_index}_adv_eps_{eps}.jpg")
-                torchvision.utils.save_image(original_img, original_path)
-                torchvision.utils.save_image(adv_img, adv_path)
+            target_dir = os.path.join(config["target_dir"], fb_attack.__class__.__name__, str(eps))
+            save_attacked_images(adversarial_images, original_images, original_image_names, target_dir)
 
 
-if __name__ == '__main__':
+def save_attacked_images(adversarial_images, original_images, original_image_names, target_dir):
+    os.makedirs(target_dir, exist_ok=True)
+    print(f"Saving original & adversarial images in directory '{target_dir}'.")
+    for original_img, adv_img, orig_image_name in zip(original_images, adversarial_images, original_image_names):
+        target_name_base = orig_image_name.item().replace(".jpg", "")
+        original_path = os.path.join(target_dir, f"{target_name_base}_orig.jpg")
+        adv_path = os.path.join(target_dir, f"{target_name_base}_adv.jpg")
+        torchvision.utils.save_image(original_img, original_path)
+        torchvision.utils.save_image(adv_img, adv_path)
+
+
+def run():
     # GPU or CPU
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    device = torch.device('cuda' if (torch.cuda.is_available() and CONFIG["use_cuda"]) else 'cpu')
 
-    # Load data
-    train_list = pd.read_csv(CONFIG["train_csv"]).sample(frac=1).values.flatten().tolist()
-    test_list = pd.read_csv(CONFIG["test_csv"]).sample(frac=1).values.flatten().tolist()
+    # transforms = transforms.Compose([transforms.RandomRotation(30),
+    #                                  transforms.RandomResizedCrop(224),
+    #                                  transforms.RandomHorizontalFlip(),
+    #                                  transforms.ToTensor(),
+    #                                  transforms.Normalize([0.485, 0.456, 0.406],
+    #                                                       [0.229, 0.224, 0.225])])
+    transform = transforms.Compose([transforms.RandomRotation(15),
+                                    transforms.RandomResizedCrop(224),
+                                    transforms.RandomHorizontalFlip(),
+                                    transforms.ToTensor()])
 
-    train_transforms = transforms.Compose([transforms.RandomRotation(30),
-                                           transforms.RandomResizedCrop(224),
-                                           transforms.RandomHorizontalFlip(),
-                                           transforms.ToTensor(),
-                                           transforms.Normalize([0.485, 0.456, 0.406],
-                                                                [0.229, 0.224, 0.225])])
+    train_dataset, validation_dataset, test_dataset = get_dogs_vs_cats_data_splits(CONFIG["train_dir"],
+                                                                                   data_split=CONFIG["data_split"],
+                                                                                   transform=transform)
+    train_loader = DataLoader(train_dataset,
+                              batch_size=CONFIG["batch_size"],
+                              shuffle=True,
+                              num_workers=CONFIG["num_workers"])
+    validation_loader = DataLoader(validation_dataset,
+                                   batch_size=CONFIG["batch_size"],
+                                   shuffle=True,
+                                   num_workers=CONFIG["num_workers"])
+    test_loader = DataLoader(test_dataset,
+                             batch_size=CONFIG["batch_size"])
 
-    # dogs_vs_cats_dataset = datasets.ImageFolder(CONFIG["train_dir"], transform=train_transforms)
-    # train_val_split = [int((1 - CONFIG["validation_split"]) * len(dogs_vs_cats_dataset)),
-    #                    int(CONFIG["validation_split"] * len(dogs_vs_cats_dataset))]
-    train_dataset, validation_dataset, test_dataset = get_dogs_vs_cats_data_splits(CONFIG["train_dir"])
-    # test_dataset = datasets.ImageFolder(CONFIG["test_dir"])
-    train_loader = DataLoader(train_dataset, batch_size=CONFIG["batch_size"],
-                              shuffle=True, num_workers=CONFIG["num_workers"])
-    validation_loader = DataLoader(validation_dataset, batch_size=CONFIG["batch_size"],
-                                   shuffle=True, num_workers=CONFIG["num_workers"])
-    # test_loader = DataLoader(test_dataset, batch_size=CONFIG["batch_size"])
-
-    images, labels = next(iter(train_loader))
+    images, labels, file_names = next(iter(test_loader))
     images = images.to(device)
     labels = labels.to(device)
+    file_names = ep.astensor(np.array(file_names))
 
     # Load model
-    model = models.vgg16(pretrained=True)
+    lit_model = LitVGG16Model.load_from_checkpoint(checkpoint_path=CONFIG["checkpoint"])
+    model = lit_model.model
     model = model.to(device)
     model.eval()
 
-    fmodel = wrap_model(model, bounds=CONFIG["bounds"], preprocessing=CONFIG["preprocessing"])
-
-    # images, labels = fb.utils.samples(fmodel, dataset='imagenet', batchsize=20)
+    # todo: do I still need to set preprocessing? DataLoader does this as well
+    fmodel = wrap_model(model, bounds=CONFIG["bounds"], preprocessing=CONFIG["preprocessing"], device=device)
 
     attack = CONFIG["attack"]
 
-    run_attack(attack, fmodel, images, labels, CONFIG)
+    run_attack(attack, fmodel, images, labels, file_names, CONFIG)
+
+
+if __name__ == '__main__':
+    run()
