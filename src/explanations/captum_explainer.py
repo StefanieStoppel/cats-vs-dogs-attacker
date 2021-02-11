c4b4dc7388bae75e7040fec3904bc4776c11b3f9
Stefanie Stoppel <s.stoppel@gmx.de>
2021-02-11 10:17:53 +0100

    feat: refactor visualization in order to be able to pass fig and axes

--- src/explanations/captum_explainer.py
+++ src/explanations/captum_explainer.py
@@ -1,7 +1,11 @@
+from typing import Union, Tuple
+
 import numpy as np
 import torch
 
 from captum.attr import visualization as viz
+from matplotlib.pyplot import axis, figure
+from util import rgb_tensor_to_pil_numpy
 
 
 class CaptumExplainer:
@@ -22,17 +26,30 @@ class CaptumExplainer:
                                                     **kwargs)
         return explanation
 
-    def visualize(self, attributions: torch.Tensor, images: torch.Tensor, titles=()):
-        attr_dl = np.transpose(attributions.cpu().detach().numpy(), (0, 2, 3, 1))
-        images_np = np.transpose((images.cpu().detach().numpy()), (0, 2, 3, 1))
+    def visualize(self,
+                  attributions: torch.Tensor,
+                  images: torch.Tensor,
+                  fig_ax_tuple: Union[None, Tuple[figure, axis]] = None,
+                  titles: Tuple = ()
+                  ):
+        attr_dl = rgb_tensor_to_pil_numpy(attributions)
+        images_np = rgb_tensor_to_pil_numpy(images)
         for idx, (img, attr) in enumerate(zip(images_np, attr_dl), 0):
             if len(titles) == 0:
                 title_ = f"Attribution for {self.xai_algorithm.__class__.__name__} {idx + 1}"
             else:
                 title_ = titles[idx]
-            _ = viz.visualize_image_attr(attr,
-                                         img,
-                                         method="blended_heat_map",
-                                         sign="all",
-                                         show_colorbar=True,
-                                         title=title_)
+            self.visualize_single(attr, img, fig_ax_tuple, title_)
+
+    def visualize_single(self,
+                         attribution: np.ndarray,
+                         image: np.ndarray,
+                         fig_ax_tuple: Union[None, Tuple[figure, axis]] = None,
+                         title: str = None):
+        return viz.visualize_image_attr(attribution,
+                                        image,
+                                        plt_fig_axis=fig_ax_tuple,
+                                        method="blended_heat_map",
+                                        sign="all",
+                                        show_colorbar=True,
+                                        title=title)
