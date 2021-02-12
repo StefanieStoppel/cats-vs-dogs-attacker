571a1ac658066320505def7d1c0ed30288072d77
Stefanie Stoppel <s.stoppel@gmx.de>
2021-02-12 11:09:58 +0100

    feat: add data set with original explanation maps

--- src/dataloader.py
+++ src/dataloader.py
@@ -3,19 +3,20 @@ import torch
 import pandas as pd
 
 from torch.utils.data import DataLoader, Dataset
-from torchvision import datasets
-
-from util import pil_read
+from torchvision import datasets, transforms
+from util import pil_read, map_explanations_backward
 
 
 class DogVsCatWithAdversarialsDataset(Dataset):
-    def __init__(self, data_txt_file, dirname, mode='train', transform=None):
+    def __init__(self, data_txt_file, dirname, mode='train', transform=None, shuffle=True):
         self.data = pd.read_csv(data_txt_file, delimiter=",", header=None, names=["orig", "orig_idx", "adv", "adv_idx"])
+        # if shuffle is True:
+        #     self.data = self.data.sample(frac=1).reset_index(drop=True)
         self.data = self.data.to_numpy()
+        self.transform = transform
         self.orig_idx, self.orig_label_idx, self.adv_idx, self.adv_label_idx = 0, 1, 2, 3
         self.dirname = dirname
         self.mode = mode
-        self.transform = transform
 
     def __len__(self):
         return len(self.data)
@@ -34,6 +35,23 @@ class DogVsCatWithAdversarialsDataset(Dataset):
             orig_img = self.transform(orig_img)
             adv_img = self.transform(adv_img)
         return orig_img, adv_img, orig_label, adv_label, orig_img_name, adv_img_name
+
+
+class DogVsCatWithOriginalExplanationsDataset(DogVsCatWithAdversarialsDataset):
+    def __init__(self, data_txt_file, dirname, mode='train', transform=None, xai_algorithm_name: str = None):
+        super().__init__(data_txt_file, dirname, mode, transform)
+        if xai_algorithm_name is None:
+            raise SystemExit("You need to specify xai_algorithmn_name!")
+        self.xai_algorithm_name = xai_algorithm_name
+
+    def __getitem__(self, idx):
+        orig_img, adv_img, orig_label, adv_label, orig_img_name, adv_img_name = \
+            super(DogVsCatWithOriginalExplanationsDataset, self).__getitem__(idx)
+        orig_explanation_path = os.path.join(self.dirname,
+                                             orig_img_name.replace("orig.jpg", f"exp_{self.xai_algorithm_name}.pt"))
+        orig_explanation_map = torch.load(orig_explanation_path, map_location=lambda storage, loc: storage)
+        orig_explanation_map.requires_grad_(False)
+        return orig_img, adv_img, orig_label, adv_label, orig_img_name, adv_img_name, orig_explanation_map
 
 
 def get_dogs_vs_cats_data_splits(data_dir, data_split=(0.7, 0.2, 0.1), transform=None, random_seed=42):
@@ -72,10 +90,3 @@ def dataset_with_file_names(cls):
     return type(cls.__name__, (cls,), {
         '__getitem__': __getitem__,
     })
-
-
-if __name__ == '__main__':
-    csv_path = "/home/steffi/dev/master_thesis/cats-vs-dogs-attacker/data/data_adv/train_adv.csv"
-    dirname = "/home/steffi/dev/master_thesis/cats-vs-dogs-attacker/data/data_adv/train"
-    dataset = DogVsCatWithAdversarialsDataset(csv_path, dirname)
-    dataset.__getitem__(2493)
